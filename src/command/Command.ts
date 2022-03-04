f235531bbb6613895c782e221e5025c8c83a22ce
AlexArcticFox <alextromb1@gmail.com>
2022-03-04 19:08:39 +0100

    Supporting slash command groups

--- src/command/Command.ts
+++ src/command/Command.ts
@@ -1,21 +1,18 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { Collection, Interaction, PermissionResolvable } from "discord.js";
-import type Subcommand from "./Subcommand";
+import type { CommandInteraction, PermissionResolvable } from "discord.js";
 
 export default abstract class Command {
     public readonly data: SlashCommandBuilder;
-    public readonly subcommands: Collection<string, Subcommand>;
     public readonly botPermissions: PermissionResolvable[];
     public readonly userPermissions: PermissionResolvable[];
 
     protected constructor(name: string, description: string, botPermissions?: PermissionResolvable[], userPermissions?: PermissionResolvable[]) {
         this.data = new SlashCommandBuilder()
             .setName(name)
-            .setDescription(description);
-        this.subcommands = new Collection();
+            .setDescription(description)
         this.botPermissions = botPermissions ?? [];
         this.userPermissions = userPermissions ?? [];
     }
 
-    public abstract execute(interaction: Interaction): void;
+    public abstract execute(interaction: CommandInteraction): void;
 }
--- src/command/CommandHandler.ts
+++ src/command/CommandHandler.ts
@@ -4,106 +4,154 @@ import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import type Command from "./Command";
 import type Subcommand from "./Subcommand";
-import { SlashCommandSubcommandBuilder } from "@discordjs/builders";
+import { SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder } from "@discordjs/builders";
 
 export default class CommandHandler {
     protected client: BotClient;
 
     public constructor(client: BotClient) {
         this.client = client;
-        const commands = getCommands(this.client);
-        loadCommands(this.client, commands);
-    }
-}
-
-function getCommands(client: BotClient): Command[] {
-    const commands = [] as Command[];
-    if (!existsSync("./dist/commands")) {
-        return commands;
+        this.getCommands();
+        this.reloadCommands();
     }
 
-    const groups = readdirSync("./dist/commands");
-    for (const group of groups) {
-        if (!statSync(`./dist/commands/${group}`)) {
-            continue;
+    getCommands(): void {
+        if (!existsSync("./dist/commands")) {
+            return;
         }
 
-        const files = readdirSync(`./dist/commands/${group}`);
-        for (const file of files) {
-            if (!file.endsWith(".js")) {
+        const commandGroupes = readdirSync("./dist/commands");
+        for (const commandGroup of commandGroupes) {
+            if (!statSync(`./dist/commands/${commandGroup}`)) {
                 continue;
             }
 
-            const path = `../commands/${group}/${file.slice(0, -3)}`;
-            let command;
+            this.readGroup(commandGroup);
+        }
+    }
 
-            try {
-                command = require(path).default;
-            } catch (error) {
-                console.log(error);
+    readGroup(group: string) {
+        const commandFiles = readdirSync(`./dist/commands/${group}`);
+        for (const commandFile of commandFiles) {
+            const commandStat = statSync(`./dist/commands/${group}/${commandFile}`);
+
+            if (commandStat.isFile() && commandFile.endsWith(".js")) {
+                this.addCommand(`../commands/${group}/${commandFile.slice(0, -3)}`);
+            } else if (commandStat.isDirectory()) {
+                const subcommandFolder = readdirSync(`./dist/commands/${group}/${commandFile}`);
+
+                for (const subcommandFile of subcommandFolder) {
+                    this.readSubcommand(group, commandFile, subcommandFile);
+                }
+            }
+        }
+    }
+
+    readSubcommand(group: string, command: string, subcommand: string) {
+        const subcommandStat = statSync(`./dist/commands/${group}/${command}/${subcommand}`);
+        if (subcommandStat.isFile() && subcommand === "index.js") {
+            this.addCommand(`../commands/${group}/${command}/${subcommand.slice(0, -3)}`);
+        } else if (subcommandStat.isFile() && subcommand.endsWith(".js")) {
+            this.addSubcommand(`../commands/${group}/${command}/${subcommand.slice(0, -3)}`, command, `../commands/${group}/${command}`);
+        } else if (subcommandStat.isDirectory()) {
+            const subcommandGroup = readdirSync(`./dist/commands/${group}/${command}/${subcommand}`);
+            const subcommands = [] as Subcommand[];
+
+            for (const subcommandGroupFile of subcommandGroup) {
+                if (subcommandGroupFile.endsWith(".js")) {
+                    subcommands.push(this.getSubcommand(`../commands/${group}/${command}/${subcommand}/${subcommandGroupFile.slice(0, -3)}`));
+                }
             }
 
-            let cmd = new command;
-            cmd = addSubCommands(cmd, `${group}/${file.slice(0, -3).toLowerCase()}`);
-            commands.push(cmd.data.toJSON());
-            client.commands.set(cmd.data.name, cmd);
+            this.addSubcommandGroup(subcommands, command, subcommand, `../commands/${group}/${command}`);
         }
     }
-    return commands;
-}
 
-export function loadCommands(client: BotClient, commands: Command[]): void {
-    const rest = new REST({ version: '9' }).setToken(client.config.token);
+    reloadCommands(): void {
+        const rest = new REST({ version: '9' }).setToken(this.client.config.token);
 
-    (async () => {
-        try {
-            console.log('Started refreshing commands.');
-
-            await rest.put(
-                Routes.applicationGuildCommands(client.config.id, client.config.guild),
-                { body: commands },
-            );
-
-            console.log('Successfully reloaded commands.');
-        } catch (error) {
-            console.error(error);
-        }
-    })();
-}
-
-function addSubCommands(command: Command, location: string): Command {
-    if (!existsSync(`./dist/commands/${location}`)) {
-        return command;
+        (async () => {
+            try {
+                const commands = this.client.commands.map(command => command.data.toJSON());
+                await rest.put(Routes.applicationGuildCommands(this.client.config.id, this.client.config.guild), { body: commands });
+                console.log('Reloaded commands');
+            } catch (error) {
+                console.error(error);
+            }
+        })();
     }
 
-    if (!statSync(`./dist/commands/${location}`)) {
-        return command;
-    }
-
-    const files = readdirSync(`./dist/commands/${location}`);
-    if (!files) {
-        return command;
-    }
-
-    for (const file of files) {
-        if (!file.endsWith(".js")) {
-            continue;
-        }
-
-        const subcommandpath = `../commands/${location}/${file.slice(0, -3)}`;
-        let subcommand;
+    getCommand(path: string): Command {
+        let commandImport;
 
         try {
-            subcommand = require(subcommandpath).default;
+            commandImport = require(path).default;
         } catch (error) {
             console.log(error);
         }
 
-        const sbc = (new subcommand) as Subcommand;
-
-        command.data.addSubcommand(new SlashCommandSubcommandBuilder().setName(sbc.data.name).setDescription(sbc.data.description));
-        command.subcommands.set(sbc.data.name, sbc);
+        return (new commandImport) as Command;
     }
 
-    return command;
+    addCommand(path: string): void {
+        const command = this.getCommand(path);
+        if (this.client.commands.get(command.data.name)) {
+            return;
+        }
+
+        this.client.commands.set(command.data.name, command);
+    }
+
+    findCommand(path: string, name: string): Command | undefined {
+        let command = this.client.commands.get(name);
+        if (!command) {
+            this.addCommand(`${path}/index`);
+            command = this.client.commands.get(name);
+            if (!command) {
+                return;
+            }
+        }
+
+        return command;
+    }
+
+    getSubcommand(path: string): Subcommand {
+        let subcommandImport;
+
+        try {
+            subcommandImport = require(path).default;
+        } catch (error) {
+            console.log(error);
+        }
+
+        return (new subcommandImport) as Subcommand;
+    }
+
+    addSubcommand(path: string, commandName: string, commandPath: string): void {
+        const command = this.findCommand(commandPath, commandName);
+        if (!command) {
+            return;
+        }
+
+        const subcommand = this.getSubcommand(path);
+
+        command.data.addSubcommand(new SlashCommandSubcommandBuilder().setName(subcommand.data.name).setDescription(subcommand.data.description));
+    }
+
+    addSubcommandGroup(subcommands: Subcommand[], commandName: string, groupName: string, commandPath: string): void {
+        const command = this.findCommand(commandPath, commandName);
+        if (!command) {
+            return;
+        }
+
+        const subcommandGroup = new SlashCommandSubcommandGroupBuilder()
+            .setName(groupName)
+            .setDescription(`group: ${groupName}`);
+
+        for (const subcommand of subcommands) {
+            subcommandGroup.addSubcommand(subcommand.data);
+        }
+
+        command.data.addSubcommandGroup(subcommandGroup);
+    }
 }
